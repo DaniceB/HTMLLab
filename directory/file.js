6ab2a3592c29cdbb2f4387c12d6eb0db306f141f
Danice Mikhaela <Danice.Biano@edu.sait.ca>
2024-11-17 21:20:54 -0700

    Create: footer, alert messages

---
+++ directory/file.js
@@ -0,0 +1,8 @@
+console.log('Hello!') ;
+
+const d = new Date();
+console.log (d.getFullYear());
+const footerYear = document.getElementById("footer");
+footerYear.textContent = d.getFullYear();
+
+alert('ᥫ᭡. You are now entering the homepage of Danice Mikhaela Biano 𓍯𓂃𓏧♡');
